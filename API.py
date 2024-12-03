6f0ff2ed171c8449432e82610e9f4d2f0b500625
Alchosamurai <alchosan.py@gmail.com>
2024-12-03 12:19:19 +0600

    аннотации

--- API.py
+++ API.py
@@ -10,14 +10,22 @@ class API:
     }
 
     @classmethod
-    def merge_telegram(cls, data):
+    def merge_telegram(cls, data: dict) -> int: 
+        """
+        Привязать телеграм к аккаунту
+        Вернет статус код
+        """
         data['api_key'] = str(config.API_KEY)  
         print(data)
         response = requests.post(f'{cls._URL}/api/merge-telegram', json=data, headers=cls._headers)
         return response.status_code
         
     @classmethod
-    def merge_phone(cls, data):
+    def merge_phone(cls, data:dict) -> int:
+        """
+        Привязать номер телефона к аккаунту
+        Вернет статус код
+        """
         data['api_key'] = str(config.API_KEY)
         response = requests.post(f'{cls._URL}/api/merge-phone', json=data, headers=cls._headers)
         return response.status_code
